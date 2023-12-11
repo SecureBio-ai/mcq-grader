44ab739b6f96b79f91549f7f117136b84d18f391
lennijusten <lenni.justen@gmail.com>
2023-12-11 12:45:20 -0500

    big update

--- run.py
+++ run.py
@@ -2,6 +2,10 @@ import argparse
 from datetime import datetime
 import os
 import pandas as pd
+from models import load_api_links, check_model_exists
+from utils import validate_input_csv, convert_df_to_mmlu_jsonl
+from pathlib import Path
+import json
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -9,24 +13,9 @@ def parse_args():
     return parser.parse_args()
 
 
-def process_input_sheet(file_path):
+def process_samplesheet(file_path):
     REQUIRED_COLUMNS = ['name', 'input', 'prompt', 'model', 'model-params']
 
-    def validate_input_csv(file_path):
-        try:
-            df = pd.read_csv(file_path)
-        except FileNotFoundError:
-            raise FileNotFoundError(f"The file {file_path} does not exist.")
-        except pd.errors.EmptyDataError:
-            raise ValueError(f"The file {file_path} is empty.")
-
-        missing_columns = [col for col in REQUIRED_COLUMNS if col not in df.columns]
-        if missing_columns:
-            raise ValueError(f"Missing required columns: {', '.join(missing_columns)}")
-        return df
-
-    def check_model(row):
-        pass
     def parse_model_params(row):
         params = row['model-params']
         if pd.isna(params) or params.strip() == '':
@@ -39,23 +28,75 @@ def process_input_sheet(file_path):
         except:
             raise ValueError("Error parsing model params. Ensure it's a valid dictionary.")
 
-    df = validate_input_csv(file_path)
+    df = validate_input_csv(file_path, REQUIRED_COLUMNS)
 
     for _, row in df.iterrows():
-        check_model(row)
+        check_model_exists(row.model)
         parse_model_params(row)
 
-    # Checks not yet implemented
+        # todo build checks to see if prompt file and input file exist
+        # todo check that input file is .tsv and prompt file is .json
+
     return df
+
+
+def check_exam(input):
+    REQUIRED_COLUMNS = ['type', 'question']
+    ALL_OPTIONS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
+
+    df = validate_input_csv(input, REQUIRED_COLUMNS, delimiter='\t')
+
+    # Check whether all question types are supported
+    with open('question-types.txt', 'r') as file:
+        supported_q_types = {line.strip() for line in file}
+
+    unsupported_types = df[~df['type'].isin(supported_q_types)]['type'].unique()
+    if len(unsupported_types) > 0:
+        print(f"WARNING: Unsupported question types found: {unsupported_types}")
+
+    # Check for missing option columns
+    max_mcq_option = max(int(q_type.split('-')[1]) for q_type in df['type'].unique() if 'MCQ' in q_type)
+    expected_options = ALL_OPTIONS[:max_mcq_option]
+    missing_columns = [opt for opt in expected_options if opt not in df.columns]
+    if missing_columns:
+        print(f"WARNING: Missing option columns {missing_columns} for the highest MCQ option MCQ-{max_mcq_option}")
+
+    # todo check that TF questions use answer option A or B not TRUE or FALSE
+
+    return df
+
+
+def question_harness(exam):
+    # For each question
+    # 1) preprocess text and check question
+    # 2) assemble prompt
+    # 3) call model
+    # 4) save answer
+    pass
+
 
 def main():
     args = parse_args()
 
-    df = process_input_sheet(args.input)
+    samplesheet = process_samplesheet(args.input)
 
-    for index, run in df.iterrows():
+    if not os.path.exists('./results'):
+        os.makedirs('./results')
+
+    for _, run in samplesheet.iterrows():
         datestring = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
-        results_path = f"{run.name}_{datestring}"
+        results_path = f"{run['name']}_{datestring}"
         os.mkdir(f"./results/{results_path}")
 
-        # ...
+        df = check_exam(run['input'])
+
+        # Convert df to mmlu-style jsonl file
+        exam_jsonl = convert_df_to_mmlu_jsonl(df, Path(run['input']).stem)
+        with open(Path(run['input']).with_suffix('.jsonl'), 'w') as file:
+            file.write(exam_jsonl)
+
+        question_harness(exam_jsonl)
+
+
+if __name__ == "__main__":
+    main()
