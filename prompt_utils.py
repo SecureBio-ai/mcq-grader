f8328d880adda51e86b48f6a6c96306880c8934c
Lennart Justen <lenni@Lennarts-MacBook-Pro.local>
2023-12-18 14:16:24 -0500

    revert commit

--- prompt_utils.py
+++ prompt_utils.py
@@ -2,6 +2,7 @@ import string
 
 
 def format_prompt(task_description, question, choices):
+    # todo implement few shot functionality
     def format_choices_by_letter(choices):
         alphabet_list = list(string.ascii_uppercase[:len(choices)])
         content = "\n".join([f"{letter}: {choice}" for letter, choice in zip(alphabet_list, choices)])
@@ -9,5 +10,5 @@ def format_prompt(task_description, question, choices):
 
     formatted_choices = format_choices_by_letter(choices)
 
-    prompt = task_description + "\n\n" + question + "\nChoices:\n" + formatted_choices + "\nAnswer: "
+    prompt = task_description + '\n' + question + '\n' + formatted_choices
     return prompt
